8c1284cbb794237a1ac44375bdc7c370f3cb927a
Rohit Sonar <rohitcsonar2000@gmail.com>
2024-10-05 17:51:57 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+
+const  JWT_SECRET ="Rohit";
+
+ const Mongo_url = "mongodb+srv://rohitsonar2000:Rohit2000@cluster0.x6lqfjc.mongodb.net/PaytymUsers"
+
+module.exports= {
+    JWT_SECRET,
+    Mongo_url
+
+}
+
